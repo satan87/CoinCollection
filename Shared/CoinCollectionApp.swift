ca5d88780d86ee5ce930927c153c57762d72c2b2
nicolas <nicolas.savoini@mac.com>
2020-06-28 16:46:49 -0400

    Initial Commit

---
+++ Shared/CoinCollectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinCollectionApp.swift
+//  Shared
+//
+//  Created by Nicolas Savoini on 2020-06-28.
+//
+
+import SwiftUI
+
+@main
+struct CoinCollectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
